86b67122e56eb6fc25190ea9b50127364828a3b7
Nikita Kamenev <kamenevnd@sibur.ru>
2024-03-05 17:13:40 +0300

    del unnecessary

--- API/settings.py
+++
@@ -1,9 +0,0 @@
-
-from envparse import Env
-
-env = Env()
-
-DATABAE_URL = env.str(
-    "DATABAE_URL",
-    default="postgresql + asyncpg://postgres:postgres@0.0.0.0:5432/postgress"
-)
--- API/simple_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import asyncio
-
-app = FastAPI()
-
-@app.get("/")
-async def read_root():
-    await asyncio.sleep(2)  # Асинхронная задержка
-    return {"message": "Вам звонит ваша МАМА! (музыка)!"}
